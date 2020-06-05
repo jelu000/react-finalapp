7969d61c54786b100a0ce4856ee5f05fd9e24057
jens l <jelu000@gmail.com>
2020-06-05 13:23:46 +0200

    la till meny

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Menucontainer from './src_kap16/Menucontainer';
 
 function App() {
   return (
     <div className="App">
       
+      < Menucontainer className="menucontainer" />
+      <h3 className="myh3">Hej</h3>
+
     </div>
   );
 }
+
+
 
 export default App;
